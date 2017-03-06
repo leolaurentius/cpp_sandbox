d5aaf7871c599d23439cc53cee3c293bdeeb5894
Lorenzo Lobba <lorenzo@lobba.it>
2017-03-06 23:18:52 +0100

    add static and dynamic cast

---
+++ dynamic_cast.cpp
@@ -0,0 +1,32 @@
+// dynamic_cast
+#include <iostream>
+#include <exception>
+using namespace std;
+
+class Base {public:    virtual void dummy() {cout<< "pippo\n" ;}};
+class Derived: public Base { int a; public: void dummy() {cout << "paperino" << endl; } };
+
+int main () {
+  try {
+    Base * pba = new Derived;
+    Base * pbb = new Base;
+    Derived * pbc = new Derived;
+
+    Derived * pd;
+    pba->dummy();
+    pd = dynamic_cast<Derived*>(pba);
+    if (pd==0) cout << "Null pointer on first type-cast.\n";
+    pd->dummy();
+
+    pd = dynamic_cast<Derived*>(pbb);
+    if (pd==0) cout << "Null pointer on second type-cast.\n";
+
+    Base * pb;
+    pb = dynamic_cast<Base*>(pbc);
+    if (pb==0) cout << "Null pointer on 3rds type-cast.\n";
+    pb->dummy();
+
+
+  } catch (exception& e) {cout << "Exception: " << e.what();}
+  return 0;
+}
