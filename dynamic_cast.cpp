732661717ddaaa1ac267eb556666da0f4e8346e3
Lorenzo Lobba <lorenzo@lobba.it>
2017-03-31 22:38:08 +0200

    add friends

--- dynamic_cast.cpp
+++ dynamic_cast.cpp
@@ -3,30 +3,44 @@
 #include <exception>
 using namespace std;
 
-class Base {public:    virtual void dummy() {cout<< "pippo\n" ;}};
-class Derived: public Base { int a; public: void dummy() {cout << "paperino" << endl; } };
+// dynamic_cast works only withh polymorph class and a class is poliymorph if
+// at least one function is virtual
+class Base { public:     void dummy() {
+		     cout << "pippo\n";
+	     }
+             virtual void pippo() {
+	     }
+};
+class Derived : public Base { int a; public: void dummy() {
+				      cout << "paperino" << endl;
+			      }
+};
 
-int main () {
-  try {
-    Base * pba = new Derived;
-    Base * pbb = new Base;
-    Derived * pbc = new Derived;
+int main() {
+	try {
+		Base *   pba = new Derived;
+		Base *   pbb = new Base;
+		Derived *pbc = new Derived;
 
-    Derived * pd;
-    pba->dummy();
-    pd = dynamic_cast<Derived*>(pba);
-    if (pd==0) cout << "Null pointer on first type-cast.\n";
-    pd->dummy();
+		Derived *pd;
+		pba->dummy();
+		pd = dynamic_cast <Derived *>(pba);
+		if (pd == 0) {
+			cout << "Null pointer on first type-cast.\n";
+		}
+		pd->dummy();
 
-    pd = dynamic_cast<Derived*>(pbb);
-    if (pd==0) cout << "Null pointer on second type-cast.\n";
+		pd = dynamic_cast <Derived *>(pbb);
+		if (pd == 0) {
+			cout << "Null pointer on second type-cast.\n";
+		}
 
-    Base * pb;
-    pb = dynamic_cast<Base*>(pbc);
-    if (pb==0) cout << "Null pointer on 3rds type-cast.\n";
-    pb->dummy();
-
-
-  } catch (exception& e) {cout << "Exception: " << e.what();}
-  return 0;
+		Base *pb;
+		pb = dynamic_cast <Base *>(pbc);
+		if (pb == 0) {
+			cout << "Null pointer on 3rds type-cast.\n";
+		}
+		pb->dummy();
+	} catch (exception& e) { cout << "Exception: " << e.what(); }
+	return(0);
 }
